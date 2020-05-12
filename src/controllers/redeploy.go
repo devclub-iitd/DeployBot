dd3376cae448b435d72b01605ea37b59f6094522
Saksham <sakshamdev5@gmail.com>
2020-05-12 12:38:01 +0530

    additional check

--- src/controllers/redeploy.go
+++ src/controllers/redeploy.go
@@ -8,16 +8,15 @@ func redeploy(callbackID string, data map[string]interface{}) {
 	stop(callbackID, data)
 	state := history.GetState(data["git_repo"].(string))
 	if state.Status == "stopped" {
-		if _, ok := data["subdomain"]; !ok {
+		if _, ok := data["subdomain"]; !ok && state.Subdomain != "" {
 			data["subdomain"] = state.Subdomain
 		}
-		if _, ok := data["access"]; !ok {
+		if _, ok := data["access"]; !ok && state.Access != "" {
 			data["access"] = state.Access
 		}
-		if _, ok := data["server_name"]; !ok {
+		if _, ok := data["server_name"]; !ok && state.Server != "" {
 			data["server_name"] = state.Server
 		}
 		deploy(callbackID, data)
 	}
-
 }
